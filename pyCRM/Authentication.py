ef3320e93dcbb9c14b01a1a484b03f3b221b0161
高文俊 <gwj337536127@163.com>
2018-08-23 16:11:17 +0800

    认证部分

--- pyCRM/Authentication.py
+++ pyCRM/Authentication.py
@@ -42,19 +42,68 @@ Base.metadata.create_all(data_conn)
 
 
 class UserAuthDao(object):
-    def __init__(self, algorithm=1):
+    def __init__(self, login_name=None, password='null', algorithm=1):
         self.__algorithm_id = algorithm
         self.__algorithm = get_algorithm(algorithm)
+        self.login_name = login_name
+        self.password_algorithm = algorithm
+        self.login_password = self.__algorithm.encoding(password)
 
-    def save(self, username, password):
+    def insert(self):
         user_auth = UserAuth()
-        user_auth.loginName = username
-        user_auth.loginPassword = self.__algorithm.encoding(password)
-        user_auth.passwordAlgorithm = self.__algorithm_id
+        user_auth.loginName = self.login_name
+        user_auth.loginPassword = self.login_password
+        user_auth.passwordAlgorithm = self.password_algorithm
+        if user_auth.loginName is None:
+            return False, None
         session = Session()
         try:
             session.add(user_auth)
             session.commit()
+            return True, None
+        except:
+            traceback.print_exc()
+            session.rollback()
+            return False, traceback.format_exc()
+        finally:
+            session.close()
+
+    def delete(self):
+        session = Session()
+        try:
+            session.query(UserAuth).filter(UserAuth.loginName == self.login_name).delete()
+            session.commit()
+            return True, None
+        except:
+            traceback.print_exc()
+            session.rollback()
+            return False, traceback.format_exc()
+        finally:
+            session.close()
+
+    def update(self, **kwargs):
+        session = Session()
+        try:
+            user_auth = session.query(UserAuth).filter(UserAuth.loginName == self.login_name).first()
+            for key in kwargs.keys():
+                if key == 'loginPassword':
+                    kwargs[key] = self.__algorithm.encoding(kwargs[key])
+                if key == 'passwordAlgorithm' or key == 'loginName' or key == 'securityId':
+                    raise Exception('不允许修改的属性')
+                setattr(user_auth, key, kwargs[key])
+            session.commit()
+            return True, None
+        except:
+            traceback.print_exc()
+            session.rollback()
+            return False, traceback.format_exc()
+        finally:
+            session.close()
+
+    def select(self):
+        session = Session()
+        try:
+            user_auth = session.query(UserAuth).filter(UserAuth.loginName == self.login_name).first()
             return True, user_auth
         except:
             traceback.print_exc()
@@ -65,9 +114,10 @@ class UserAuthDao(object):
 
 
 class UserAuthService(object):
-    pass
+    def login(self, username, password):
+        pass
 
 
 if __name__ == '__main__':
-    userAuthDao = UserAuthDao()
-    print(userAuthDao.save('gao', '123')[1])
+    userAuthDao = UserAuthDao('aaaa', '123')
+    print(userAuthDao.select()[1].createDate)
---
+++ pyCRM/Error.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+@Module Name: Error
+@Author: gao
+@Contact: gwj337536127@163.com
+@Create date: 2018/8/23 9:52
+@Description:
+-------------------------------------------------
+@Change Activity:
+    2018/8/23:创建文件
+-------------------------------------------------
+"""
+
+__author__ = 'gao'
+
+import traceback
+
+from pyCRM import data_conn
+from pyCRM import Session
+
+from functools import wraps
+
+from sqlalchemy import func
+from sqlalchemy import Column, BigInteger, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class ErrorMessage(Base):
+    """
+    错误信息,用于存放系统中的各种错误代码及错误详细信息
+    @param errorID:错误代码
+    @param message:错误信息
+    """
+    __tablename__ = 'ErrorMessage'
+    errorID = Column(BigInteger, primary_key=True)
+    message = Column(Text)
+
+
+Base.metadata.create_all(data_conn)
+
+
+class ErrorDao(object):
+    def __init__(self, error_id=0, message='null'):
+        self.error_message = ErrorMessage()
+        self.error_message.errorID = error_id
+        self.error_message.message = message
+
+    def insert(self):
+        session = Session()
+        count = session.query(func.count('*')).filter(ErrorMessage.errorID == self.error_message.errorID).scalar()
+        try:
+            if count != 0:
+                em = session.query(ErrorMessage).filter(ErrorMessage.errorID == self.error_message.errorID).first()
+                em.message = self.error_message.message
+                session.commit()
+            else:
+                session.add(self.error_message)
+                session.commit()
+            return True, self.error_message
+        except:
+            traceback.print_exc()
+            session.rollback()
+            return False, traceback.format_exc()
+        finally:
+            session.close()
+
+    def get(self, error_id):
+        session = Session()
+        try:
+            self.error_message = session.query(ErrorMessage).filter(ErrorMessage.errorID == error_id).first()
+            return True, self.error_message
+        except:
+            traceback.print_exc()
+            return False, traceback.format_exc()
+        finally:
+            session.close()
+
+
+class ErrorService(object):
+    @staticmethod
+    def insert_error(error_id, message):
+        error_dao = ErrorDao(error_id, message)
+        return error_dao.insert()
+
+    @staticmethod
+    def get_error(error_id):
+        error_dao = ErrorDao()
+        return error_dao.get(error_id)
+
+
+def insert_error_wapper(error_id, message='null'):
+    ErrorService.insert_error(error_id, message)
+
+    def method_wapper(method):
+        @wraps(method)
+        def wapper(*args, **kwargs):
+            return method(*args, **kwargs)
+
+        return wapper
+
+    return method_wapper
