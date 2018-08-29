4fbd9fdbf75f39596986544e4da0a748dbb19172
heron <liujianxiong@runmost.com>
2018-08-29 19:09:59 +0800

    首次提交,用户信息管理模块文件

---
+++ pyCRM/UserInfoManageModule.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+@Module Name: UserInfoManageModule
+@Author: heron
+@Contact: gwj337536127@163.com
+@Create date: 2018/8/29 17:54
+@Description:
+-------------------------------------------------
+@Change Activity:
+    2018/8/29:创建文件
+-------------------------------------------------
+"""
+__author__ = 'heron'
