239d81319af9493e488c4a33f9c78cff92a1755a
gao337536127 <Gao123159>
2018-09-03 18:02:10 +0800

    默认路径修改

--- pyCRM/__init__.py
+++ pyCRM/__init__.py
@@ -7,8 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 os.environ["NLS_LANG"] = "SIMPLIFIED CHINESE_CHINA.ZHS16GBK"
 
-sql_url = r'sqlite:///D:\PycharmProjects\pyCRM\dev.db'
-sql_task_url = r'sqlite:///D:\PycharmProjects\pyCRM\task.db'
+main_path = os.path.dirname(os.path.dirname(__file__))
+sql_url = os.path.join(main_path, 'dev.db')
+sql_task_url = os.path.join(main_path, 'task.db')
 
 # data_conn = create_engine('oracle://gao:gao123159@172.17.254.200:1521/mydev', echo=True, pool_size=10)
 data_conn = create_engine(sql_url, echo=True)
